4a9a7e669baf61cd1d9226d6f5b2a42773d78c53
thomasyosh <thomas.yosh@tutanota.com>
2025-05-26 13:05:43 +0000

    change lowercase of table

--- ITP4915M/Data/Entity/Appointment.cs
+++ ITP4915M/Data/Entity/Appointment.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace ITP4915M.Data.Entity
 {
 
-   [Table("Appointment")]
+   [Table("appointment")]
     public class Appointment
     {
         [Key]
