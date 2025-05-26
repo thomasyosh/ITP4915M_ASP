52e32d2ef6943691189d03339a1ee66c0776fa11
thomasyosh <149293032+thomasyosh@users.noreply.github.com>
2025-05-26 11:07:16 +0000

    mysql

--- ITP4915M/Program.cs
+++ ITP4915M/Program.cs
@@ -3,21 +3,33 @@
     using System.Text;
     using Microsoft.AspNetCore.Authentication.JwtBearer;
     using MySqlConnector;
+    using ITP4915M.Data;
+    using ITP4915M.Helpers.File;
+    using ITP4915M.Helpers.Extension;
+    using ITP4915M.AppLogic.Exceptions;
+    using ITP4915M.Helpers.LogHelper;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.OpenApi.Models;
+    using Swashbuckle.AspNetCore.Filters;
 
-    public class Program {
-    private static void Main (string [] args) {
+    public class Program
+{
+    private static void Main(string[] args)
+    {
         var builder = WebApplication.CreateBuilder(args);
         // Add services to the container.
-
-        builder.Services.AddControllers();
-        // builder.Services.AddDbContext<DataContext>(options =>
-        // {
-        //     var ConnString = Environment.GetEnvironmentVariable("ConnectionString");
-        //     options.UseMySql(
-        //         ConnString,
-        //         ServerVersion.AutoDetect(ConnString)
-        //     );
-        // });
+        builder.Services.AddControllers()
+            .AddNewtonsoftJson(
+                options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
+        builder.Services.AddDbContext<DataContext>(options =>
+        {
+            var ConnString = Environment.GetEnvironmentVariable("ConnectionString");
+            options.UseMySql(
+                ConnString,
+                ServerVersion.AutoDetect(ConnString)
+            );
+        });
 
         builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer(options =>
@@ -31,20 +43,76 @@
                     ValidateAudience = false
                 };
             });
-
+        
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+            {
+                Description = "Auth using the bearer",
+                In = ParameterLocation.Header,
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey
+            });
+            options.OperationFilter<SecurityRequirementsOperationFilter>();
+        });
+        builder.Services.AddCors(
+            options =>
+            {
+                options.AddPolicy("default",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader();
+                    });
+            }
+        );
+        
         var app = builder.Build();
-        app.UseSwagger();
-        app.UseSwaggerUI();
-        app.UseHttpsRedirection();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseAuthentication();
         app.UseAuthorization();
         app.MapControllers();
-        app.Run();
+        app.UseCors("default");
+        Console.Title = "The Better Limited Server";
+
+        string ConnStringParts = Environment.GetEnvironmentVariable("ConnectionString");
+        using (var conn = new MySqlConnection(ConnStringParts))
+        {
+            try // test the connection with sql server
+            {
+                conn.Open();
+            }
+            catch (MySqlException e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Timeout: {conn.ConnectionTimeout}s\nError occur during create a connection with MySQL server");
+                Console.WriteLine("\t-Make sure you start the MySQL server");
+                Console.WriteLine("\t-Make sure your connection string is correct");
+                Console.WriteLine("\t-Make sure you have the right permissions");
+                return;
+            }
+            finally
+            {
+                conn.Close();
+                Console.ResetColor();
+            }
+        }
+
+        ConsoleLogger.Debug("Version");
+
+        app.Run("");
+
+        TempFileManager.CloseAllTempFile();
+        ITP4915M.Helpers.File.PDFFactory.Instance.Dispose();
     }
 }
-
-
-
--- student-record-management/Program.cs
+++ student-record-management/Program.cs
@@ -1,123 +1,46 @@
-using ITP4915M.Data;
-using ITP4915M.Helpers.LogHelper;
-using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using myWebApp.Models;
+using myWebApp.Data;
 
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+// Add services to the container.
+builder.Services.AddDbContext<SchoolContext>(options =>
+   options.UseNpgsql(builder.Configuration.GetConnectionString("SchoolContext")));
+
+var app = builder.Build();
+using (var scope = app.Services.CreateScope())
 {
-    private static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-        // Add services to the container.
-        builder.Services.AddControllers()
-            .AddNewtonsoftJson(
-                options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-            );
-        // builder.Services.AddDbContext<DataContext>(options =>
-        // {
-        //     var ConnString = _Secret["ConnectionString"];
-        //     options.UseMySql(
-        //         ConnString,
-        //         ServerVersion.AutoDetect(ConnString)
-        //     );
-        // });
+   var services = scope.ServiceProvider;
+   try
+   {
+       // add 10 seconds delay to ensure the db server is up to accept connections
+       // this won't be needed in real world application
+       System.Threading.Thread.Sleep(10000);
+       var context = services.GetRequiredService<SchoolContext>();
+       var created = context.Database.EnsureCreated();
 
-        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>
-            {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
-                        .GetBytes("hihi")),
-                    ValidateIssuer = false,
-                    ValidateAudience = false
-                };
-            });
-        
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen(options =>
-        {
-            options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
-            {
-                Description = "Auth using the bearer",
-                In = ParameterLocation.Header,
-                Name = "Authorization",
-                Type = SecuritySchemeType.ApiKey
-            });
-            options.OperationFilter<SecurityRequirementsOperationFilter>();
-        });
-        builder.Services.AddCors(
-            options =>
-            {
-                options.AddPolicy("default",
-                    builder =>
-                    {
-                        builder.AllowAnyOrigin()
-                            .AllowAnyMethod()
-                            .AllowAnyHeader();
-                    });
-            }
-        );
-        
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseAuthentication();
-        app.UseAuthorization();
-        app.MapControllers();
-        app.UseCors("default");
-        Console.Title = "The Better Limited Server";
-
-        string ConnStringParts = "";
-        // using (var conn = new MySqlConnection(TestConnString.ToString()))
-        // {
-        //     try // test the connection with sql server
-        //     {
-        //         conn.Open();
-        //     }
-        //     catch (MySqlException e)
-        //     {
-        //         Console.ForegroundColor = ConsoleColor.Red;
-        //         Console.WriteLine($"Timeout: {conn.ConnectionTimeout}s\nError occur during create a connection with MySQL server");
-        //         Console.WriteLine("\t-Make sure you start the MySQL server");
-        //         Console.WriteLine("\t-Make sure your connection string is correct");
-        //         Console.WriteLine("\t-Make sure you have the right permissions");
-        //         return;
-        //     }
-        //     finally
-        //     {
-        //         conn.Close();
-        //         Console.ResetColor();
-        //     }
-        // }
-
-// #if DEBUG
-//         using (var serviceScope = app.Services.CreateScope())
-//         {
-//             var dbContext = serviceScope.ServiceProvider.GetRequiredService<DataContext>();
-//             // drop the database if it exists
-//             dbContext.Database.ExecuteSqlRaw(
-//                 "DROP DATABASE IF EXISTS `TheBetterLimitedDev`;"
-//             );
-//             // create the database
-//             dbContext.Database.Migrate();
-//             // insert some dummy data
-//             ITP4915M.Data.DummyDataFactory.Create(dbContext).GetAwaiter().GetResult();
-//         }
-// #endif
-
-        ConsoleLogger.Debug("Version");
-
-        app.Run();
-
-        TempFileManager.CloseAllTempFile();
-        ITP4915M.Helpers.File.PDFFactory.Instance.Dispose();
-    }
+   }
+   catch (Exception ex)
+   {
+       var logger = services.GetRequiredService<ILogger<Program>>();
+       logger.LogError(ex, "An error occurred creating the DB.");
+   }
 }
+
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
