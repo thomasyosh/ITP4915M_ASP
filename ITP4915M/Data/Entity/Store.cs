eb44ce26ccbba185205528de91bd8acdd732f862
thomasyosh <thomas.yosh@tutanota.com>
2025-06-01 15:57:22 +0800

    fixed ef

--- ITP4915M/Data/Entity/Store.cs
+++ ITP4915M/Data/Entity/Store.cs
@@ -1,5 +1,6 @@
-using Microsoft.EntityFrameworkCore;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ITP4915M.Data.Entity
 {
